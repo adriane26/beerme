f7a1550c67f5a7ad2eb0bcde2d418f494aa5c588
adriane26 <adrianepurdy@yahoo.com>
2015-12-18 10:46:19 -0800

    updated script.js

--- public/js/script.js
+++ public/js/script.js
@@ -9,21 +9,21 @@
  //  window.location = url;
  //  });
 
-//  $(document).ready(function() {
-//   $('.delete-btn').click(function(e) {
-//     e.preventDefault();
-//     var url = $(this).attr('href');
-//     var well = $(this).parent();
-//     $.ajax({
-//       url: url,
-//       method: 'DELETE'
-//     }).done(function(data) {
-//       console.log(data);
-//       if (data.msg === 'success') {
-//         well.fadeOut(2000, function() {
-//           well.remove();
-//         });
-//       }
-//     })
-//   });
-// });
+ $(document).ready(function() {
+  $('.delete-btn').click(function(e) {
+    e.preventDefault();
+    var url = $(this).attr('href');
+    var well = $(this).parent();
+    $.ajax({
+      url: url,
+      method: 'DELETE'
+    }).done(function(data) {
+      console.log(data);
+      if (data.msg === 'success') {
+        well.fadeOut(2000, function() {
+          well.remove();
+        });
+      }
+    })
+  });
+});
